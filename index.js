f6c3aa19579a93d1e534219e56df6c5b89ec645f
PasanSasmika <pasansasmika198@gmail.com>
2024-12-19 08:11:42 +0530

    🛢️Server Connected to the mongoDb🍃

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const express = require('express')
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
 const app = express()
 const port = process.env.PORT || 5000;
 
+const mongoUrl = process.env.MONGO_URI
 
+mongoose.connect(mongoUrl,{})
+
+const connection = mongoose.connection;
+
+connection.once("open",()=>{
+  console.log("Database connected");
+})
+
+
+
+//middleware
+
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req,res)=>{
 
@@ -12,3 +33,6 @@ app.get('/', (req,res)=>{
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
 })
+
+
+
